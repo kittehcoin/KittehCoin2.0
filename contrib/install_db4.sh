9d12989df7c6878ec56b8877bc3b8b8bda3a7d33
ryandanielt <ryan@ryanmcphee.net>
2026-08-17 12:22:29 -0400

    Update install_db4.sh

--- contrib/install_db4.sh
+++ contrib/install_db4.sh
@@ -20,7 +20,8 @@ expand_path() {
   cd "${1}" && pwd -P
 }
 
-BDB_PREFIX="$(expand_path ${1})/db4"; shift;
+BASE_DIR="$(expand_path "${1}")"
+BDB_PREFIX="${BASE_DIR}/db4"; shift;
 BDB_VERSION='db-4.8.30.NC'
 BDB_HASH='12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef'
 BDB_URL="https://download.oracle.com/berkeley-db/${BDB_VERSION}.tar.gz"
@@ -68,26 +69,38 @@ tar -xzvf ${BDB_VERSION}.tar.gz -C "$BDB_PREFIX"
 cd "${BDB_PREFIX}/${BDB_VERSION}/"
 
 # Apply a patch necessary when building with clang and c++11 (see https://community.oracle.com/thread/3952592)
-CLANG_CXX11_PATCH_URL='https://gist.githubusercontent.com/LnL7/5153b251fd525fe15de69b67e63a6075/raw/7778e9364679093a32dec2908656738e16b6bdcb/clang.patch'
-CLANG_CXX11_PATCH_HASH='7a9a47b03fd5fb93a16ef42235fa9512db9b0829cfc3bdf90edd3ec1f44d637c'
-http_get "${CLANG_CXX11_PATCH_URL}" clang.patch "${CLANG_CXX11_PATCH_HASH}"
-patch -p2 < clang.patch
+CLANG_PATCH="${BASE_DIR}/depends/patches/bdb/clang_cxx_11.patch"
+if [ -f "${CLANG_PATCH}" ]; then
+  echo "Applying in-tree clang C++11 patch: ${CLANG_PATCH}"
+  patch -p1 < "${CLANG_PATCH}"
+else
+  CLANG_CXX11_PATCH_URL='https://gist.githubusercontent.com/LnL7/5153b251fd525fe15de69b67e63a6075/raw/7778e9364679093a32dec2908656738e16b6bdcb/clang.patch'
+  CLANG_CXX11_PATCH_HASH='7a9a47b03fd5fb93a16ef42235fa9512db9b0829cfc3bdf90edd3ec1f44d637c'
+  http_get "${CLANG_CXX11_PATCH_URL}" clang.patch "${CLANG_CXX11_PATCH_HASH}"
+  patch -p2 < clang.patch
+fi
 
 # The packaged config.guess and config.sub are ancient (2009) and can cause build issues.
-# Replace them with modern versions.
+# Prefer in-tree depends copies; Savannah gitweb is often flaky / returns HTML errors.
 # See https://github.com/bitcoin/bitcoin/issues/16064
-CONFIG_GUESS_URL='https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=55eaf3e779455c4e5cc9f82efb5278be8f8f900b'
-CONFIG_GUESS_HASH='2d1ff7bca773d2ec3c6217118129220fa72d8adda67c7d2bf79994b3129232c1'
-CONFIG_SUB_URL='https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=55eaf3e779455c4e5cc9f82efb5278be8f8f900b'
-CONFIG_SUB_HASH='3a4befde9bcdf0fdb2763fc1bfa74e8696df94e1ad7aac8042d133c8ff1d2e32'
-
 rm -f "dist/config.guess"
 rm -f "dist/config.sub"
 
-http_get "${CONFIG_GUESS_URL}" dist/config.guess "${CONFIG_GUESS_HASH}"
-http_get "${CONFIG_SUB_URL}" dist/config.sub "${CONFIG_SUB_HASH}"
+if [ ! -f "${BASE_DIR}/depends/config.guess" ] || [ ! -f "${BASE_DIR}/depends/config.sub" ]; then
+  echo "Missing ${BASE_DIR}/depends/config.guess or config.sub" >&2
+  echo "Run this script from a full kittehcoin checkout (contrib/install_db4.sh \`pwd\`)." >&2
+  exit 1
+fi
+echo "Using in-tree depends/config.guess and depends/config.sub"
+cp -f "${BASE_DIR}/depends/config.guess" dist/config.guess
+cp -f "${BASE_DIR}/depends/config.sub" dist/config.sub
+chmod +x dist/config.guess dist/config.sub
 
 cd build_unix/
+
+# Match depends/packages/bdb.mk: silence implicit-declaration errors on modern Clang.
+export CFLAGS="${CFLAGS:-} -Wno-error=implicit-function-declaration"
+export CXXFLAGS="${CXXFLAGS:-} -std=c++11"
 
 "${BDB_PREFIX}/${BDB_VERSION}/dist/configure" \
   --enable-cxx --disable-shared --disable-replication --with-pic --prefix="${BDB_PREFIX}" \
